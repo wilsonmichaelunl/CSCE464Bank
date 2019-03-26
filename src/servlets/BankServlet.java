9a719d7a83ff1b41b8fe58f852ebb3cf654c19c1
Michael Wilson <wilson.michael@huskers.unl.edu>
2019-03-25 19:56:29 -0500

    WIP order servlet

--- src/servlets/BankServlet.java
+++ src/servlets/BankServlet.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import database.BankDB;
 import helpers.MonthYearFormatter;
 import models.Bank;
@@ -22,6 +24,8 @@ import models.Bank;
 @WebServlet("/BankServlet")
 public class BankServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	static Logger log 
+    = Logger.getLogger(BankServlet.class.getName());
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -35,43 +39,44 @@ public class BankServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
 		MonthYearFormatter format = new MonthYearFormatter();
 		Bank bank = new Bank();
 		BankDB dbBank = new BankDB();
 		int validBank = 0;
 		
-		int userId = Integer.parseInt(request.getParameter("userId"));
-		String lastName = request.getParameter("lastName").trim();
-		String firstName = request.getParameter("firstName").trim();
-		String cardType = request.getParameter("cardType").trim();
-		String cardNumber = request.getParameter("cardNumber".trim());
-		String cvv = request.getParameter("securityNumber").trim();
-		String expirationMonth = format.formatMonth(request.getParameter("expirationDateMonth").trim());
-		String expirationYear = format.formatYear(request.getParameter("expirationDateYear").trim());
-		String expirationDate = format.formatExpirationDate(expirationMonth, expirationYear);
-		String billingAddress = request.getParameter("billingAddress").trim();
-		String shippingAddress = request.getParameter("shippingAddress").trim();
-		Number numOrderTotal = 0;
 		try {
-			numOrderTotal = NumberFormat.getInstance().parse(request.getParameter("shoppingCartTotal"));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		BigDecimal orderTotal = new BigDecimal(numOrderTotal.toString());
-		
-		Bank creditCard = dbBank.getCreditCardByUserId(userId);
-		int validDetails = bank.validateDetails(creditCard, lastName, firstName, cardType, cardNumber, cvv, expirationDate);
-		
-		if(validDetails == 1) {
-			int validBalance = bank.validateBalance(creditCard, orderTotal);
+			int userId = Integer.parseInt(request.getParameter("userId"));
+			String lastName = request.getParameter("lastName").trim();
+			String firstName = request.getParameter("firstName").trim();
+			String cardType = request.getParameter("cardType").trim();
+			String cardNumber = request.getParameter("cardNumber".trim());
+			String cvv = request.getParameter("securityNumber").trim();
+			String expirationMonth = format.formatMonth(request.getParameter("expirationDateMonth").trim());
+			String expirationYear = format.formatYear(request.getParameter("expirationDateYear").trim());
+			String expirationDate = format.formatExpirationDate(expirationMonth, expirationYear);
+			String billingAddress = request.getParameter("billingAddress").trim();
+			String shippingAddress = request.getParameter("shippingAddress").trim();
+			Number numOrderTotal = 0;
+		    numOrderTotal = NumberFormat.getInstance().parse(request.getParameter("shoppingCartTotal"));
+			BigDecimal orderTotal = new BigDecimal(numOrderTotal.toString());
 			
-			if(validBalance == 1) {
-				validBank = 1;
+			Bank creditCard = dbBank.getCreditCardByUserId(userId);
+			int validDetails = bank.validateDetails(creditCard, lastName, firstName, cardType, cardNumber, cvv, expirationDate);
+			
+			if(validDetails == 1) {
+				int validBalance = bank.validateBalance(creditCard, orderTotal);
+				
+				if(validBalance == 1) {
+					validBank = 1;
+				}
 			}
+		}catch(Exception e) {
+			out.println("Exception/Error information is written on the log file.");
+			log.error("This is an error message: ", e);	
 		}
 		
-		PrintWriter out = response.getWriter();
+		
 		out.println(validBank);
 	}
 
--- src/servlets/PlaceOrdersServlet.java
+++ src/servlets/PlaceOrdersServlet.java
@@ -1,11 +1,21 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import database.BankDB;
+import helpers.MonthYearFormatter;
+import models.Bank;
 
 /**
  * Servlet implementation class PlaceOrdersServlet
@@ -13,6 +23,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/PlaceOrdersServlet")
 public class PlaceOrdersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	static Logger log 
+    = Logger.getLogger(PlaceOrdersServlet.class.getName());
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -26,8 +38,23 @@ public class PlaceOrdersServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		Bank bank = new Bank();
+		BankDB dbBank = new BankDB();
+		String jsonCart = request.getParameter("jsonCart");
+		System.out.println("in place order servlet: " + jsonCart);
+		
+//		try {
+//			int userId = Integer.parseInt(request.getParameter("userId"));
+//			Number numOrderTotal = 0;
+//		    numOrderTotal = NumberFormat.getInstance().parse(request.getParameter("shoppingCartTotal"));
+//			BigDecimal orderTotal = new BigDecimal(numOrderTotal.toString());
+//			Bank creditCard = dbBank.getCreditCardByUserId(userId);
+//		}catch(Exception e) {
+//			out.println("Exception/Error information is written on the log file.");
+//			log.error("This is an error message: ", e);	
+//		}
+
 	}
 
 	/**
